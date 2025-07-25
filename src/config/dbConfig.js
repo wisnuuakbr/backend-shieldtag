fd048e95700c3b78b8c7c36a6a94f0bfa4e0c53e
wisnuuakbr <wisnuaero3@gmail.com>
2025-07-25 22:52:34 +0700

    chore(config): add database config file

---
+++ src/config/dbConfig.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+require("dotenv").config();
+
+const dbConfig = new Sequelize({
+    logging: false,
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+module.exports = dbConfig;
